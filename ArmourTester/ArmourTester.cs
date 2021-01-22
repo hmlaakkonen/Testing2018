913da97a23f35cfe1ad449c1e1f7bed83a24e720
Heikki Laakkonen <l5082@student.jamk.fi>
2021-01-22 20:01:32 +0200

    Added a unit test class and test methods there.

---
+++ ArmourTester/ArmourTester.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing2018;
+
+namespace ArmourTester
+{
+    [TestClass]
+    public class ArmourTester
+    {
+        [TestMethod]
+        public void testLevel()
+        {
+            Armour ar = new Armour("Kalle", "Kustaa", 30, 2, 1);
+            int lvl = ar.getLevel();
+
+            if (lvl < 0)
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void testSlot()
+        {
+            Armour ar = new Armour("Kalle", "Kustaa", 30, 2, 1);
+            int slot = ar.getSlot();
+                
+            if (slot < 0 || slot > 3) {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void testHeadSlot()
+        {
+            Armour ar = new Armour("Kalle", "Kypäri", 30, 2, 1);
+            int slot = ar.getSlot();
+
+            Assert.AreEqual(0, slot);
+        }
+    }
+}
---
+++ ArmourTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArmourTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArmourTester")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85d0ac04-1333-46e3-bd54-9d1f14aff672")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
